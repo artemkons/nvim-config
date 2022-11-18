10033b4484f36a8ac5efe5f636dede5791d7c6e7
artemkons <konstantinov050701@gmail.com>
2022-11-18 16:16:08 +0300

    Update config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,6 +22,7 @@ vim.cmd([[
         autocmd!
         autocmd ColorScheme * :highlight BufferTabpageFill ctermbg=none
         autocmd ColorScheme * :highlight BufferCurrentMod ctermfg=1
+        autocmd ColorScheme * :highlight VertSplit ctermbg=none
     augroup END
 ]])
 
@@ -41,7 +42,7 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<leader>ws", ":split <cr>", opts)
 keymap("n", "<leader>wv", ":vsplit <cr>", opts)
 
-keymap("", "<leader>f", ":NERDTreeToggle <cr>", opts)
+keymap("", "<leader>f", ":Lf <cr>", opts)
 
 -- Doesn't work with keymap func, idk why
 local builtin = require('telescope.builtin')
@@ -66,24 +67,102 @@ return require('packer').startup(function(use)
 		config = "vim.cmd('colorscheme wal')"
 	}
 
+vim.cmd("set fillchars=vert:\\ ")
+vim.cmd("")
+
 use 'mhinz/vim-startify'
 
-	use {
-		'preservim/nerdtree',
-		config = function()
-			vim.cmd('let NERDTreeShowHidden = 1')
-		end
-	}
+use {
+    'is0n/fm-nvim',
+    config = function()
+        require('fm-nvim').setup{
+        -- (Vim) Command used to open files
+        edit_cmd = "edit",
 
-	use {
-		'Xuyuanp/nerdtree-git-plugin',
-		requires = 'preservim/nerdtree'
-	}
+        -- See `Q&A` for more info
+        on_close = {},
+        on_open = {},
+
+        -- UI Options
+        ui = {
+            -- Default UI (can be "split" or "float")
+            default = "float",
+
+            float = {
+                -- Floating window border (see ':h nvim_open_win')
+                border    = "none",
+
+                -- Highlight group for floating window/border (see ':h winhl')
+                float_hl  = "Normal",
+                border_hl = "FloatBorder",
+
+                -- Floating Window Transparency (see ':h winblend')
+                blend     = 0,
+
+                -- Num from 0 - 1 for measurements
+                height    = 0.8,
+                width     = 0.8,
+
+                -- X and Y Axis of Window
+                x         = 0.5,
+                y         = 0.5
+            },
+
+            split = {
+                -- Direction of split
+                direction = "topleft",
+
+                -- Size of split
+                size      = 24
+            }
+        },
+
+        -- Terminal commands used w/ file manager (have to be in your $PATH)
+        cmds = {
+            lf_cmd      = "lf", -- eg: lf_cmd = "lf -command 'set hidden'"
+            fm_cmd      = "fm",
+            nnn_cmd     = "nnn",
+            fff_cmd     = "fff",
+            twf_cmd     = "twf",
+            fzf_cmd     = "fzf", -- eg: fzf_cmd = "fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+            fzy_cmd     = "find . | fzy",
+            xplr_cmd    = "xplr",
+            vifm_cmd    = "vifm",
+            skim_cmd    = "sk",
+            broot_cmd   = "broot",
+            gitui_cmd   = "gitui",
+            ranger_cmd  = "ranger",
+            joshuto_cmd = "joshuto",
+            lazygit_cmd = "lazygit",
+            neomutt_cmd = "neomutt",
+            taskwarrior_cmd = "taskwarrior-tui"
+        },
+
+        -- Mappings used with the plugin
+        mappings = {
+            vert_split = "<C-v>",
+            horz_split = "<C-h>",
+            tabedit    = "<C-t>",
+            edit       = "<C-e>",
+            ESC        = ":quit <cr>"
+        },
+
+        -- Path to broot config
+        broot_conf = vim.fn.stdpath("data") .. "/site/pack/packer/start/fm-nvim/assets/broot_conf.hjson"
+    }
+
+
+  end
+}
 
 	use {
 		'TimUntersberger/neogit',
 		requires = 'nvim-lua/plenary.nvim'
 	}
+
+keymap("n", "<leader>gg", ":Neogit <cr>", opts)
+
+keymap("n", "<leader>gb", ":CocCommand git.showBlameDoc <cr>", opts)
 
     use {
       'romgrk/barbar.nvim',
@@ -93,6 +172,10 @@ use 'mhinz/vim-startify'
         }
       end
     }
+
+keymap("n", "<leader>t]", ":BufferNext <cr>", opts)
+keymap("n", "<leader>t[", ":BufferPrevious <cr>", opts)
+keymap("n", "<leader>tq", ":BufferClose <cr>", opts)
 
     use 'vim-airline/vim-airline'
 
@@ -145,10 +228,53 @@ use {
     requires = "neovim/nvim-lspconfig"
 }
 
+use {
+  'mfussenegger/nvim-dap',
+  config=function()
+    local dap = require('dap')
+        dap.adapters.firefox = {
+        type = 'executable',
+        command = 'node',
+        args = {os.getenv('HOME') .. '/vscode-firefox-debug/dist/adapter.bundle.js'},
+    }
+
+    dap.configurations.typescriptreact = {
+      {
+        name = 'Debug with Firefox',
+        type = 'firefox',
+        request = 'launch',
+        reAttach = true,
+        url = 'http://localhost:3000',
+        webRoot = '${workspaceFolder}',
+        firefoxExecutable = '/Applications/Firefox.app/Contents/MacOS/firefox'
+      }
+    }
+  end
+ }
+
 	use {
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate'
 	}
+
+use {
+  'm-demare/hlargs.nvim',
+  requires = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+      require('hlargs').setup()
+      require('hlargs').enable()
+  end
+}
+
+use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
 
 	use 'junegunn/limelight.vim'
 	use 'junegunn/goyo.vim'
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -90,10 +90,22 @@ _G.packer_plugins = {
     path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/coc.nvim",
     url = "https://github.com/neoclide/coc.nvim"
   },
+  ["fm-nvim"] = {
+    config = { "\27LJ\2\n¥\6\0\0\6\0\22\0\0296\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0004\3\0\0=\3\4\0024\3\0\0=\3\5\0025\3\6\0005\4\a\0=\4\b\0035\4\t\0=\4\n\3=\3\v\0025\3\f\0=\3\r\0025\3\14\0=\3\15\0026\3\16\0009\3\17\0039\3\18\3'\5\19\0B\3\2\2'\4\20\0&\3\4\3=\3\21\2B\0\2\1K\0\1\0\15broot_conf</site/pack/packer/start/fm-nvim/assets/broot_conf.hjson\tdata\fstdpath\afn\bvim\rmappings\1\0\5\ftabedit\n<C-t>\15horz_split\n<C-h>\15vert_split\n<C-v>\bESC\15:quit <cr>\tedit\n<C-e>\tcmds\1\0\17\20taskwarrior_cmd\20taskwarrior-tui\ffff_cmd\bfff\fnnn_cmd\bnnn\vfm_cmd\afm\vlf_cmd\alf\16neomutt_cmd\fneomutt\16lazygit_cmd\flazygit\16joshuto_cmd\fjoshuto\15ranger_cmd\vranger\14gitui_cmd\ngitui\14broot_cmd\nbroot\rskim_cmd\ask\rvifm_cmd\tvifm\rxplr_cmd\txplr\ffzy_cmd\17find . | fzy\ffzf_cmd\bfzf\ftwf_cmd\btwf\aui\nsplit\1\0\2\tsize\3\24\14direction\ftopleft\nfloat\1\0\b\nwidth\4š³æÌ\t™³¦ÿ\3\14border_hl\16FloatBorder\6x\4\0€€€ÿ\3\rfloat_hl\vNormal\6y\4\0€€€ÿ\3\vborder\tnone\vheight\4š³æÌ\t™³¦ÿ\3\nblend\3\0\1\0\1\fdefault\nfloat\fon_open\ron_close\1\0\1\redit_cmd\tedit\nsetup\ffm-nvim\frequire\0" },
+    loaded = true,
+    path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/fm-nvim",
+    url = "https://github.com/is0n/fm-nvim"
+  },
   ["goyo.vim"] = {
     loaded = true,
     path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/goyo.vim",
     url = "https://github.com/junegunn/goyo.vim"
+  },
+  ["hlargs.nvim"] = {
+    config = { "\27LJ\2\nO\0\0\3\0\4\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0016\0\0\0'\2\1\0B\0\2\0029\0\3\0B\0\1\1K\0\1\0\venable\nsetup\vhlargs\frequire\0" },
+    loaded = true,
+    path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/hlargs.nvim",
+    url = "https://github.com/m-demare/hlargs.nvim"
   },
   ["limelight.vim"] = {
     loaded = true,
@@ -111,16 +123,11 @@ _G.packer_plugins = {
     path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/neogit",
     url = "https://github.com/TimUntersberger/neogit"
   },
-  nerdtree = {
-    config = { "\27LJ\2\n>\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\31let NERDTreeShowHidden = 1\bcmd\bvim\0" },
+  ["nvim-dap"] = {
+    config = { "\27LJ\2\n±\3\0\0\a\0\r\0\0216\0\0\0'\2\1\0B\0\2\0029\1\2\0005\2\4\0004\3\3\0006\4\5\0009\4\6\4'\6\a\0B\4\2\2'\5\b\0&\4\5\4>\4\1\3=\3\t\2=\2\3\0019\1\n\0004\2\3\0005\3\f\0>\3\1\2=\2\v\1K\0\1\0\1\0\a\burl\26http://localhost:3000\rreAttach\2\frequest\vlaunch\22firefoxExecutable5/Applications/Firefox.app/Contents/MacOS/firefox\tname\23Debug with Firefox\fwebRoot\23${workspaceFolder}\ttype\ffirefox\20typescriptreact\19configurations\targs1/vscode-firefox-debug/dist/adapter.bundle.js\tHOME\vgetenv\aos\1\0\2\fcommand\tnode\ttype\15executable\ffirefox\radapters\bdap\frequire\0" },
     loaded = true,
-    path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/nerdtree",
-    url = "https://github.com/preservim/nerdtree"
-  },
-  ["nerdtree-git-plugin"] = {
-    loaded = true,
-    path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/nerdtree-git-plugin",
-    url = "https://github.com/Xuyuanp/nerdtree-git-plugin"
+    path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    url = "https://github.com/mfussenegger/nvim-dap"
   },
   ["nvim-lspconfig"] = {
     config = { "\27LJ\2\n,\0\2\6\0\2\0\0066\2\0\0009\2\1\2\18\4\0\0\18\5\1\0B\2\3\1K\0\1\0\vattach\nnavic\\\1\0\4\0\a\0\n6\0\0\0'\2\1\0B\0\2\0029\0\2\0009\0\3\0005\2\5\0003\3\4\0=\3\6\2B\0\2\1K\0\1\0\14on_attach\1\0\0\0\nsetup\vclangd\14lspconfig\frequire\0" },
@@ -132,6 +139,12 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/nvim-navic",
     url = "https://github.com/SmiteshP/nvim-navic"
+  },
+  ["nvim-surround"] = {
+    config = { "\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0" },
+    loaded = true,
+    path = "/Users/konstantinov050701gmail.com/.local/share/nvim/site/pack/packer/start/nvim-surround",
+    url = "https://github.com/kylechui/nvim-surround"
   },
   ["nvim-treesitter"] = {
     loaded = true,
@@ -184,30 +197,42 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: barbar.nvim
-time([[Config for barbar.nvim]], true)
-try_loadstring("\27LJ\2\nF\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\nicons\1\nsetup\15bufferline\frequire\0", "config", "barbar.nvim")
-time([[Config for barbar.nvim]], false)
--- Config for: telescope-project.nvim
-time([[Config for telescope-project.nvim]], true)
-try_loadstring("\27LJ\2\nL\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0'\2\3\0B\0\2\1K\0\1\0\fproject\19load_extension\14telescope\frequire\0", "config", "telescope-project.nvim")
-time([[Config for telescope-project.nvim]], false)
+-- Config for: hlargs.nvim
+time([[Config for hlargs.nvim]], true)
+try_loadstring("\27LJ\2\nO\0\0\3\0\4\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0016\0\0\0'\2\1\0B\0\2\0029\0\3\0B\0\1\1K\0\1\0\venable\nsetup\vhlargs\frequire\0", "config", "hlargs.nvim")
+time([[Config for hlargs.nvim]], false)
 -- Config for: nvim-lspconfig
 time([[Config for nvim-lspconfig]], true)
 try_loadstring("\27LJ\2\n,\0\2\6\0\2\0\0066\2\0\0009\2\1\2\18\4\0\0\18\5\1\0B\2\3\1K\0\1\0\vattach\nnavic\\\1\0\4\0\a\0\n6\0\0\0'\2\1\0B\0\2\0029\0\2\0009\0\3\0005\2\5\0003\3\4\0=\3\6\2B\0\2\1K\0\1\0\14on_attach\1\0\0\0\nsetup\vclangd\14lspconfig\frequire\0", "config", "nvim-lspconfig")
 time([[Config for nvim-lspconfig]], false)
--- Config for: nerdtree
-time([[Config for nerdtree]], true)
-try_loadstring("\27LJ\2\n>\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\31let NERDTreeShowHidden = 1\bcmd\bvim\0", "config", "nerdtree")
-time([[Config for nerdtree]], false)
--- Config for: vim-airline-themes
-time([[Config for vim-airline-themes]], true)
-try_loadstring("\27LJ\2\nD\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0%let g:airline_theme='jellybeans'\bcmd\bvim\0", "config", "vim-airline-themes")
-time([[Config for vim-airline-themes]], false)
+-- Config for: barbar.nvim
+time([[Config for barbar.nvim]], true)
+try_loadstring("\27LJ\2\nF\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\nicons\1\nsetup\15bufferline\frequire\0", "config", "barbar.nvim")
+time([[Config for barbar.nvim]], false)
+-- Config for: fm-nvim
+time([[Config for fm-nvim]], true)
+try_loadstring("\27LJ\2\n¥\6\0\0\6\0\22\0\0296\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0004\3\0\0=\3\4\0024\3\0\0=\3\5\0025\3\6\0005\4\a\0=\4\b\0035\4\t\0=\4\n\3=\3\v\0025\3\f\0=\3\r\0025\3\14\0=\3\15\0026\3\16\0009\3\17\0039\3\18\3'\5\19\0B\3\2\2'\4\20\0&\3\4\3=\3\21\2B\0\2\1K\0\1\0\15broot_conf</site/pack/packer/start/fm-nvim/assets/broot_conf.hjson\tdata\fstdpath\afn\bvim\rmappings\1\0\5\ftabedit\n<C-t>\15horz_split\n<C-h>\15vert_split\n<C-v>\bESC\15:quit <cr>\tedit\n<C-e>\tcmds\1\0\17\20taskwarrior_cmd\20taskwarrior-tui\ffff_cmd\bfff\fnnn_cmd\bnnn\vfm_cmd\afm\vlf_cmd\alf\16neomutt_cmd\fneomutt\16lazygit_cmd\flazygit\16joshuto_cmd\fjoshuto\15ranger_cmd\vranger\14gitui_cmd\ngitui\14broot_cmd\nbroot\rskim_cmd\ask\rvifm_cmd\tvifm\rxplr_cmd\txplr\ffzy_cmd\17find . | fzy\ffzf_cmd\bfzf\ftwf_cmd\btwf\aui\nsplit\1\0\2\tsize\3\24\14direction\ftopleft\nfloat\1\0\b\nwidth\4š³æÌ\t™³¦ÿ\3\14border_hl\16FloatBorder\6x\4\0€€€ÿ\3\rfloat_hl\vNormal\6y\4\0€€€ÿ\3\vborder\tnone\vheight\4š³æÌ\t™³¦ÿ\3\nblend\3\0\1\0\1\fdefault\nfloat\fon_open\ron_close\1\0\1\redit_cmd\tedit\nsetup\ffm-nvim\frequire\0", "config", "fm-nvim")
+time([[Config for fm-nvim]], false)
 -- Config for: lsp_lines.nvim
 time([[Config for lsp_lines.nvim]], true)
 try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14lsp_lines\frequire\0", "config", "lsp_lines.nvim")
 time([[Config for lsp_lines.nvim]], false)
+-- Config for: vim-airline-themes
+time([[Config for vim-airline-themes]], true)
+try_loadstring("\27LJ\2\nD\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0%let g:airline_theme='jellybeans'\bcmd\bvim\0", "config", "vim-airline-themes")
+time([[Config for vim-airline-themes]], false)
+-- Config for: nvim-surround
+time([[Config for nvim-surround]], true)
+try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0", "config", "nvim-surround")
+time([[Config for nvim-surround]], false)
+-- Config for: nvim-dap
+time([[Config for nvim-dap]], true)
+try_loadstring("\27LJ\2\n±\3\0\0\a\0\r\0\0216\0\0\0'\2\1\0B\0\2\0029\1\2\0005\2\4\0004\3\3\0006\4\5\0009\4\6\4'\6\a\0B\4\2\2'\5\b\0&\4\5\4>\4\1\3=\3\t\2=\2\3\0019\1\n\0004\2\3\0005\3\f\0>\3\1\2=\2\v\1K\0\1\0\1\0\a\burl\26http://localhost:3000\rreAttach\2\frequest\vlaunch\22firefoxExecutable5/Applications/Firefox.app/Contents/MacOS/firefox\tname\23Debug with Firefox\fwebRoot\23${workspaceFolder}\ttype\ffirefox\20typescriptreact\19configurations\targs1/vscode-firefox-debug/dist/adapter.bundle.js\tHOME\vgetenv\aos\1\0\2\fcommand\tnode\ttype\15executable\ffirefox\radapters\bdap\frequire\0", "config", "nvim-dap")
+time([[Config for nvim-dap]], false)
+-- Config for: telescope-project.nvim
+time([[Config for telescope-project.nvim]], true)
+try_loadstring("\27LJ\2\nL\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0'\2\3\0B\0\2\1K\0\1\0\fproject\19load_extension\14telescope\frequire\0", "config", "telescope-project.nvim")
+time([[Config for telescope-project.nvim]], false)
 -- Config for: wal.vim
 time([[Config for wal.vim]], true)
 vim.cmd('colorscheme wal')
